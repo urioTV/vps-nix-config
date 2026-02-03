8cb35973b5f6f377e68c6f3c17693eafe1649f02
urioTV <uriootv@protonmail.com>
2026-02-03 08:36:02 +0100

    feat: update flake inputs and integrate the home-manager flake module.

--- flake.nix
+++ flake.nix
@@ -33,6 +33,10 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.home-manager.flakeModules.home-manager
+      ];
+
       systems = [
         "x86_64-linux"
         "aarch64-linux"
